3a36b70831e06b80305a019bfcc59b372a779fa1
LubomirP <lubomirpacheliev@gmail.com>
2022-02-01 14:04:31 +0200

    webpack & ts configs

---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+export default {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.tsx'),
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['tsx', 'jsx', 'ts', 'js']
+  },
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js',
+  },
+};
