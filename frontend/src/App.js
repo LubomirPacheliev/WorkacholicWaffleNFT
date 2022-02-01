0b3571415b067948234a75a4c5999ec2712644d4
LubomirP <lubomirpacheliev@gmail.com>
2022-02-01 11:35:56 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="connect-wallet-header">
+        <h1>Hello, gamer 👻</h1>
+        <p>why don't you connect your phantom wallet</p>
+        <button className="connect-wallet-btn draw">connect</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
