1ebd2567446cb50a073ad2df7a16b248eef4e686
LubomirP <lubomirpacheliev@gmail.com>
2022-02-01 19:12:04 +0200

    some phantom connect header tests

--- frontend/src/components/ConnectHeader/ConnectHeader.test.tsx
+++ frontend/src/components/ConnectHeader/ConnectHeader.test.tsx
@@ -3,9 +3,22 @@
  */
 
 import React from "react";
-import { render, getByText, fireEvent } from "@testing-library/react";
+import { render, getByText, fireEvent, screen } from "@testing-library/react";
 import ConnectHeader from './ConnectHeader';
 
 test("Should be rendering properly", () => {
     render(<ConnectHeader />);
+});
+
+test("Solana is NOT in window", () => { // known bug
+    // @ts-ignore
+    const solana = window.solana;
+    expect(solana).toBe(undefined);
+});
+
+test("Btn requests connection on click", () => {
+    render(<ConnectHeader />);
+    const btn = document.createElement('button');
+    const connect = screen.getByText('connect');
+    fireEvent.click(connect);
 });
