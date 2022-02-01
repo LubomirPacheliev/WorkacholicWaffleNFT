e01f19a1c65fdca2827151e1866a6e1b1fa2d870
LubomirP <lubomirpacheliev@gmail.com>
2022-02-01 14:05:22 +0200

    ts app & index

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ConnectHeader from "./components/LandingPage/ConnectHeader";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ConnectHeader />
+    </div>
+  );
+}
+
+export default App;
