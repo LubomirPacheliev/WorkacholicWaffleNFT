3674c886c8dc08718e3d0a20784da0a8c30623da
LubomirP <lubomirpacheliev@gmail.com>
2022-02-02 18:56:39 +0200

    typical entrypoint

--- token/src/lib.rs
+++ token/src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
+    msg, pubkey::Pubkey, program_error::ProgramError
+};
+
+entrypoint!(process_instructions);
+
+fn process_instructions(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    msg!("Shutuuuup! Dumb!");
+    Ok(())
 }
